d2a162dff22bc82e4dc8d8b1efedfe2849ec78c8
Andre Sampaio <andrebsampaio@gmail.com>
2019-01-02 17:46:34 +0100

    Minor UI changes + started product details page

--- app/src/main/java/com/reviewedbuy/HomeActivity.java
+++ app/src/main/java/com/reviewedbuy/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.reviewedbuy;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -7,9 +8,15 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    AutoCompleteTextView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
+
+        startActivity(new Intent(this, ProductPageActivity.class));
+
+        mSearchView = findViewById(R.id.autoCompleteTextView);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle =
@@ -31,10 +42,22 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         if (navigationView != null) {
             navigationView.setNavigationItemSelectedListener(this);
         }
+
+        mSearchView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEARCH){
+                    //TODO - search for stuff
+                }
+                return false;
+            }
+        });
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
     }
+
+
 }
---
+++ app/src/main/java/com/reviewedbuy/ProductPageActivity.java
@@ -0,0 +1,70 @@
+package com.reviewedbuy;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductPageActivity extends AppCompatActivity {
+    ViewPager mViewPager;
+    TabLayout mTabLayout;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_page_main);
+
+        mViewPager = findViewById(R.id.viewpager);
+        setupViewPager(mViewPager);
+
+        mTabLayout = findViewById(R.id.tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ProductDetailsFragment(), "DETAILS");
+        adapter.addFragment(new ProductReviewsFragment(), "REVIEWS");
+        adapter.addFragment(new ProductReviewsFragment(), "" );
+
+        viewPager.setAdapter(adapter);
+    }
+
+
+}
+
+class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
+
+
